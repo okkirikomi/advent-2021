67106636cb52eec1c7284d8a3bde32e6e6a56bed
Martin <github@okkirikomi.com>
2021-12-09 09:31:31 -0500

    Day 9 - Part two

--- src/day9.cpp
+++ src/day9.cpp
@@ -1,31 +1,105 @@
+#include <queue>
 #include <stdio.h>
 
 #include "file.h"
+#include "radix.h"
 #include "strtoint.h"
 #include "timer.h"
 
 static const uint8_t INPUT_MAX = 128;
 static const uint8_t MAX_ROW = 100;
 static const uint8_t N_COL = 100;
+static const uint8_t MAX_POINTS = 0xFF;
 
+typedef struct Pos {
+    uint8_t x, y;
+} Pos;
 
 typedef struct Heightmap {
     void init();
-    uint32_t low_points_risk() const;
+    uint16_t low_points_risk();
+    uint32_t largest_basins();
     bool add_row(const char* str);
 
 private:
+    Pos _low_points[MAX_POINTS];
     uint8_t _map[MAX_ROW][N_COL];
     uint8_t _n_row;
+    uint8_t _n_lows;
 
 } Heightmap;
 
 void Heightmap::init() {
     _n_row = 0;
+    _n_lows = 0;
 }
 
-uint32_t Heightmap::low_points_risk() const {
-    uint32_t risk = 0;
+uint32_t Heightmap::largest_basins() {
+    uint16_t a = 0;
+    uint16_t b = 0;
+    uint16_t c = 0;
+    for (uint8_t i = 0; i < _n_lows; ++i) {
+        uint16_t basin_size = 1;
+        // FIXME, get rid of std::queue
+        std::queue<Pos> queue;
+        // we set to 9 to mark visited
+        _map[_low_points[i].y][_low_points[i].x] = 9;
+        queue.push(std::move(_low_points[i]));
+
+        // visit neighbours as long as they are not 9
+        while (!queue.empty()) {
+            Pos visiting = std::move(queue.front());
+            queue.pop();            
+            if (visiting.x != 0 && 9 != _map[visiting.y][visiting.x-1]) {
+                Pos to_visit;
+                to_visit.x = visiting.x-1;
+                to_visit.y = visiting.y;
+                _map[to_visit.y][to_visit.x] = 9;
+                queue.push(std::move(to_visit));
+                basin_size += 1;
+            }
+            if (visiting.y != 0 && 9 != _map[visiting.y-1][visiting.x]) {
+                Pos to_visit;
+                to_visit.x = visiting.x;
+                to_visit.y = visiting.y-1;
+                _map[to_visit.y][to_visit.x] = 9;
+                queue.push(std::move(to_visit));
+                basin_size += 1;
+            }
+            if (visiting.x < N_COL - 1 && 9 != _map[visiting.y][visiting.x+1]) {
+                Pos to_visit;
+                to_visit.x = visiting.x+1;
+                to_visit.y = visiting.y;
+                _map[to_visit.y][to_visit.x] = 9;
+                queue.push(std::move(to_visit));
+                basin_size += 1;
+            }
+            if (visiting.y < _n_row - 1 && 9 != _map[visiting.y+1][visiting.x]) {
+                Pos to_visit;
+                to_visit.x = visiting.x;
+                to_visit.y = visiting.y+1;
+                _map[to_visit.y][to_visit.x] = 9;
+                queue.push(std::move(to_visit));
+                basin_size += 1;
+            }
+        }
+        // BETTER, got to be a better way
+        if(basin_size > a) {
+            c = b;
+            b = a;
+            a = basin_size;
+        } else if(basin_size > b) {
+            c = b;
+            b = basin_size;
+        } else if(basin_size > c) {
+            c = basin_size;
+        }
+    }
+    return a * b * c;
+}
+
+uint16_t Heightmap::low_points_risk() {
+    uint16_t risk = 0;
     for (uint8_t y = 0; y < _n_row; ++y) {
         for (uint8_t x = 0; x < N_COL; ++x) {
             // check the 4 neighbors
@@ -43,6 +117,11 @@ uint32_t Heightmap::low_points_risk() const {
                 continue;
 
             risk += 1 + height;
+
+            if (_n_lows == MAX_POINTS) continue;
+            _low_points[_n_lows].x = x;
+            _low_points[_n_lows].y = y;
+            ++_n_lows;
         }
     }
     return risk;
@@ -91,7 +170,7 @@ int main(int argc, char **argv)
     }
 
     const uint16_t answer1 = height_map.low_points_risk();
-    const uint32_t answer2 = 0;
+    const uint32_t answer2 = height_map.largest_basins();
 
     file.close();
 
